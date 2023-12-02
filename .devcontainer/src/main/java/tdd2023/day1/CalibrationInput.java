0bc7b7d4bb3acd21330547cd0c8e8dd9e30b2d68
andreeaniculcea <147836843+andreeaniculcea@users.noreply.github.com>
2023-12-02 17:50:40 +0000

    refactoring

--- .devcontainer/src/main/java/tdd2023/day1/CalibrationInput.java
+++ .devcontainer/src/main/java/tdd2023/day1/CalibrationInput.java
@@ -10,10 +10,7 @@ public record CalibrationInput(String input) {
 
     // extract digits into list and the extract the calibration value based on how many digits there are
     public int calculateCalibrationValue() {
-        List<String> digits = Arrays.stream(input.replaceAll("[^0-9]", "")
-                .split(""))
-                .filter(s -> !s.isEmpty())
-                .collect(Collectors.toList());
+        List<String> digits = extractDigits(input);
 
         System.out.println(digits);
         if (digits.size() == 0) {
@@ -25,6 +22,14 @@ public record CalibrationInput(String input) {
         System.out.println(firstDigit + lastDigit);
         return Integer.parseInt(firstDigit + lastDigit);
 
+    }
+
+    private List<String> extractDigits(String inputString) {
+        List<String> digits = Arrays.stream(inputString.replaceAll("[^0-9]", "")
+                .split(""))
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
+        return digits;
     }
 
 
